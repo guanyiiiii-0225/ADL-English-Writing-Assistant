c0b3d8ec31bcb22796d283e0d952154687330ae1
timicienio <you@example.com>
2023-12-15 15:00:03 +0000

    feature: chinese llama 2 grading

--- essay_grading/inference.py
+++ essay_grading/inference.py
@@ -16,112 +16,125 @@ torch.set_printoptions(threshold=10_000)
 
 def get_system_prompt():
     criteria = {
-        "內容 (滿分 5 分)": {
-            "5 或 4 分": "主題(句)清楚切題，並有具體、完整的相關細節支持。",
-            "3 分": "主題不夠清楚或突顯，部分相關敘述發展不全。",
-            "2 或 1 分": "主題不明，大部分相關敘述發展不全或與主題無關。",
-            "0 分": "文不對題或沒寫。",
+        "Content (out of 5 points)": {
+            "5 or 4": "Clear subject(s) and backed by specific, complete related details.",
+            "3": "The subject is not clear or emphasized, and some related descriptions are not fully developed.",
+            "2 or 1": "The subject is unclear, most related descriptions are undeveloped or unrelated to the main point.",
+            "0": "Off-topic or not written at all.",
         },
-        "組織 (滿分 5 分)": {
-            "5 或 4 分": "重點分明，有開頭、發展、結尾，前後連貫，轉承語使用得當。",
-            "3 分": "重點安排不妥，前後發展比例與轉承語使用欠妥。",
-            "2 或 1 分": "重點不明、前後不連貫。",
-            "0 分": "全文毫無組織或未按提示寫作。",
+        "Organization (out of 5 points)": {
+            "5 or 4": "Key points are clear, with an introduction, development, and conclusion. The transitions are used properly.",
+            "3": "Key points are not well arranged, and the use of development examples and transitions is inappropriate.",
+            "2 or 1": "Key points are unclear, and there's a lack of coherence.",
+            "0": "The entire text lacks organization, or did not follow the prompts for writing.",
         },
-        "文法、句構 (滿分 4 分)": {
-            "4 分": "全文幾無文法錯誤，文句結構富變化。",
-            "3 分": "文法錯誤少，且未影響文意之表達。",
-            "2 或 1 分": "文法錯誤多，且明顯影響文意之表達。",
-            "0 分": "全文文法錯誤嚴重，導致文意不明。",
+        "Grammar, Sentence Structure (out of 4 points)": {
+            "4": "Almost no grammatical errors in the text, and the sentence structure varies.",
+            "3": "Few grammatical errors that do not affect the expression of the text's meaning.",
+            "2 or 1": "Many grammar errors that significantly affect the expression of the text's meaning.",
+            "0": "Severe grammar errors throughout the text, making the meaning unclear.",
         },
-        "字彙、拼字 (滿分 4 分)": {
-            "4 分": "用字精確、得宜，且幾無文法錯誤。",
-            "3 分": "字詞單調、重複，用字偶有不當，少許拼字錯誤，但不影響文意之表達。",
-            "2 或 1 分": "用字、拼字錯誤多，明顯影響文意之表達。",
-            "0 分": "只寫出或抄襲與題意有關的零碎字詞。",
+        "Vocabulary, Spelling (out of 4 points)": {
+            "4": "Precise and appropriate word choice, with almost no grammatical errors.",
+            "3": "Monotonous/repetitive vocabulary, occasional inappropriate word choice, few spelling errors that do not affect the expression of the text's meaning.",
+            "2 or 1": "Many errors in word choice and spelling that significantly affect the text's meaning.",
+            "0": "Only wrote or plagiarized fragmented words related to the topic.",
         },
-        "體例 (滿分 2 分)": {
-            "2 分": "格式、標點、大小寫幾無錯誤。",
-            "1 分": "格式、標點、大小寫等有錯誤，但不影響文意之表達。",
-            "0 分": "違背基本的寫作體例或格式，標點、大小寫等錯誤甚多。",
+        "Format (out of 2 points)": {
+            "2": "The format, punctuation, and capitalization are nearly flawless.",
+            "1": "Errors in the format, punctuation, and capitalization, but they do not affect the expression of the text's meaning.",
+            "0": "Violates basic writing format or conventions, with numerous errors in punctuation, capitalization, and so on.",
         },
     }
     criteria_string = json.dumps(criteria, ensure_ascii=False,  indent=4)
     criteria_formatted = re.sub('[\"{},]', "", criteria_string)
-    prompt = f"你是一場考試中的英語作文評分者。\n這場考試的評分項目有「內容」、「組織」、「文法、句構」、「字彙、拼字」、「體例」五項，每個項目有不同的配分，評分標準如下：{criteria_formatted}\n"
+    prompt = f"You are an English essay grader in an examination.\nThe scoring items in this exam are 'Content', 'Organization', 'Grammar, Sentence Structure', 'Vocabulary, Spelling', and 'Format'. Each item has different scores, and the scoring standards are as follows: {criteria_formatted}.\n"
 
     return prompt
 
 def get_example_prompt(problem: str, content: str, output: str):
-    prompt = f"[INST]以下是這場考試的主題：{problem}\n以下是要進行評分的作文內容：\n{content}。\n\n請閱讀此作文，為各評分項目打分數：[/INST]\n{output}\n"
+    prompt = f"[INST]The following is the topic of this exam: {problem}\nThe following is the essay content to be graded:\n{content}.\n\nPlease read this essay and score each grading item: [/INST]\n{output}</s>\n"
     # print(prompt)
     return prompt
 
 def get_user_prompt(problem: str, content: str):
-    prompt = f"[INST]以下是這場考試的主題：{problem}\n以下是要進行評分的作文內容：\n{content}。\n\n請閱讀此作文，為各評分項目打分數：[/INST]\n"
+    prompt = f"[INST]The following is the topic of this exam: {problem}\nThe following is the essay content to be graded:\n{content}.\n\nPlease read this essay and score each grading item: [/INST]\n"
     return prompt
 
 def get_prompt(problem, content, examples):
     example_indices = [0, 1, 2]
     prompt = f"<s>[INST] <<SYS>>{get_system_prompt()}<</SYS>>[/INST]{''.join([get_example_prompt(examples[ind]['problem'], examples[ind]['content'], examples[ind]['output']) for ind in example_indices])}{get_user_prompt(problem, content)}"
-    # print(prompt)
+    print(prompt)
     return prompt
 
 def inference_raw(pipe, tokenizer, problem, content, examples):
     prompt = get_prompt(problem, content, examples)
-    # tokenized_instruction = tokenizer(prompt, padding=True, return_tensors='pt').to('cuda')
-    # output = model.generate(**tokenized_instruction, 
-    #                         generation_config=GenerationConfig(
-    #                             do_sample=True,
-    #                             max_new_tokens=2048,
-    #                             top_p=0.99,
-    #                             temperature=0.001,
-    #                         ),
-    #                         pad_token_id=tokenizer.eos_token_id
-    #                     )
-
     sequences = pipe(
         prompt,
         do_sample=True,
-        top_k=10,
+        top_p=0.99,
+        temperature=1e-8,
         num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        pad_token_id=tokenizer.pad_token_id,
+        # eos_token_id=tokenizer.eos_token_id,
+        # pad_token_id=tokenizer.pad_token_id,
         max_new_tokens=1024
     )
 
     output = sequences[0]["generated_text"].rsplit('[/INST]')[-1].strip()
-    # print(output)
     
     return output
 
-# def inference(model, tokenizer, problem, content):
+# def inference(model, tokenizer, problem, content, examples):
 #     json_schema = {
 #         "type": "object",
 #         "properties": {
-#             "分數": {
+#             "內容 (滿分 5 分)": {
 #                 "type": "object",
 #                 "properties": {
-#                     "內容 (滿分 5 分)": {"type": "number"},
-#                     "組織 (滿分 5 分)": {"type": "number"},
-#                     "文法、句構 (滿分 4 分)": {"type": "number"},
-#                     "字彙、拼字 (滿分 4 分)": {"type": "number"},
-#                     "體例 (滿分 2 分)": {"type": "number"},
-#                 },
+#                     "分數": {"type": "number"},
+#                     "評語": {"type": "string"},
+#                 }
 #             },
-#             # "評語": {"type": "string"},
+#             "組織 (滿分 5 分)":  {
+#                 "type": "object",
+#                 "properties": {
+#                     "分數": {"type": "number"},
+#                     "評語": {"type": "string"},
+#                 }
+#             },
+#             "文法、句構 (滿分 4 分)":  {
+#                 "type": "object",
+#                 "properties": {
+#                     "分數": {"type": "number"},
+#                     "評語": {"type": "string"},
+#                 }
+#             },
+#             "字彙、拼字 (滿分 4 分)":  {
+#                 "type": "object",
+#                 "properties": {
+#                     "分數": {"type": "number"},
+#                     "評語": {"type": "string"},
+#                 }
+#             },
+#             "體例 (滿分 2 分)":  {
+#                 "type": "object",
+#                 "properties": {
+#                     "分數": {"type": "number"},
+#                     "評語": {"type": "string"},
+#                 }
+#             },
 #         },
 #     }
 
-#     prompt = get_prompt(problem, content)
-#     jsonformer = Jsonformer(model, tokenizer, json_schema, prompt, temperature=1e-8)
+#     prompt = get_prompt(problem, content, examples)
+#     jsonformer = Jsonformer(model, tokenizer, json_schema, prompt, temperature=0.01)
 #     generated_data = jsonformer()
 
 #     return generated_data
 
 
 def grade(test_data_path, example_data_path, output_path):
-    model_name = "hfl/chinese-llama-2-7b-16k"
+    model_name = "togethercomputer/LLaMA-2-7B-32K"
 
     quant_config = BitsAndBytesConfig(
         load_in_4bit=True,
@@ -138,7 +151,7 @@ def grade(test_data_path, example_data_path, output_path):
 
     dataset = load_dataset("json", data_files={"test": test_data_path, "example": example_data_path})
 
-    tokenizer = AutoTokenizer.from_pretrained("hfl/chinese-llama-2-7b-16k", use_fast=False)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
 
     model = AutoModelForCausalLM.from_pretrained(
         model_name, quantization_config=quant_config, torch_dtype=torch.bfloat16
@@ -156,10 +169,16 @@ def grade(test_data_path, example_data_path, output_path):
 
     answers = []
 
-    for x in dataset["test"]:
+    for x in dataset["example"]:
+        # output = inference(model, tokenizer, x["problem"], x["content"], dataset["example"])
         output = inference_raw(pipe, tokenizer, x["problem"], x["content"], dataset["example"])
         print(output)
-        answers.append(output)
+        print('')
+        answers.append({
+            "problem": x["problem"],
+            "content": x["content"],
+            "output": output
+        })
     
     of = open(output_path, 'w', encoding='utf8')
     json.dump(answers, of, ensure_ascii=False)
