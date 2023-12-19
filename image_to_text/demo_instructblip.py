357023b976d4612d339b74ad3b4f784e5505f2b6
Guanshen77 <a1598875787@gmail.com>
2023-12-19 20:57:45 +0800

    feature: generate text of images

--- image_to_text/demo_instructblip.py
+++ image_to_text/demo_instructblip.py
@@ -3,28 +3,38 @@ import torch
 from PIL import Image
 import requests
 
-def generate_image_description(image_path, prompt="Can you describe this image in detail?"):
-    model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b", load_in_8bit=True, device_map={"": 0}, torch_dtype=torch.bfloat16)
+def generate_image_description(images_paths, prompt="Can you describe this image in detail?"):
+    
     processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
+    
+    description_arr = []
+    for image_path in images_paths:
+        #image = Image.open(image_path).convert('RGB')
+        # convert the image to text
+        #image_description = "This is an image."
+        with torch.no_grad():
+            image = Image.open(image_path).convert("RGB")
+            model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b", load_in_8bit=True, device_map={"": 0}, torch_dtype=torch.bfloat16)
+            inputs = processor(images=image, text=prompt, return_tensors="pt").to(device="cuda", dtype=torch.bfloat16)
+            outputs = model.generate(
+                **inputs,
+                do_sample=False,
+                num_beams=3,
+                max_length=256,
+                min_length=1,
+                top_p=0.9,
+                repetition_penalty=1.5,
+                length_penalty=1.0,
+                temperature=1,
+            )
+            generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
+            image_description = generated_text
+            print(image_description)
+            description_arr.append(image_description)
+        del model
+        torch.cuda.empty_cache()
+    
 
-    image = Image.open(image_path).convert("RGB")
-    inputs = processor(images=image, text=prompt, return_tensors="pt").to(device="cuda", dtype=torch.bfloat16)
 
-    outputs = model.generate(
-        **inputs,
-        do_sample=False,
-        num_beams=5,
-        max_length=256,
-        min_length=1,
-        top_p=0.9,
-        repetition_penalty=1.5,
-        length_penalty=1.0,
-        temperature=1,
-    )
-    generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
-    return generated_text
+    return description_arr
 
-# Example usage:
-image_path = "./103_4.png"
-generated_description = generate_image_description(image_path)
-print(generated_description)
--- streamlit.py
+++ streamlit.py
@@ -1,36 +1,41 @@
 import streamlit as st
 from PIL import Image
-
+import re
 from grammar_checker.inter import generate_html
 from grammar_checker.extract2 import extract_from_html
 from image_to_text.demo_instructblip import generate_image_description
-from exemplar_generator.inference.generate_writing import generate_writing_without_finetune
 
 def image_to_text(images_paths):
+
+    description_arr = generate_image_description(images_paths)
+    '''
     description_arr = []
     for image_path in images_paths:
-        image = Image.open(image_path).convert('RGB')
+        #image = Image.open(image_path).convert('RGB')
         # convert the image to text
-        image_description = "This is an image."
+        #image_description = "This is an image."
+        #print(image_path)
+        image_description = 'i am description'
+        #print(image_description)
         description_arr.append(image_description)
-
+    '''
     return description_arr
 
 def extend_question(question, image_description_arr):
     # add the image descriptions to the question
-    if len(image_description_arr) == 1:
+    question += "\n\n"
+    for desc in image_description_arr:
+        question += desc
         question += "\n\n"
-        question += f"Figure: {image_description_arr[0]}"
-    else:
-        for idx, desc in enumerate(image_description_arr):
-            question += "\n\n"
-            question += f"Figure {idx + 1}: {desc}"
     
     return question
 
 def generate_example(question):
+    # wait 1 seconds
+    import time
+    time.sleep(1)
     # generate the example answer
-    example = generate_writing_without_finetune(question)
+    example = "I agree with the trend of universities encouraging professors to teach professional courses in English. This approach has several benefits. Firstly, it exposes students to a broader range of academic literature and research, as English is the lingua franca of higher education. This helps students to develop a more nuanced understanding of their field and stay up-to-date with the latest developments. Secondly, teaching in English promotes a more diverse and inclusive learning environment, as students from different countries and backgrounds can participate and engage with the course materials more easily.\n\nIf I were to enroll in a future university where the required courses are taught in English, I would embrace this opportunity to improve my language skills and broaden my academic horizons. I would take advantage of resources such as language support services and peer mentorship programs to help me succeed in these courses. Additionally, I would actively seek out opportunities to engage with course materials and participate in class discussions, using my knowledge of English to contribute to the learning environment. Overall, I believe that teaching professional courses in English is a valuable approach that can enhance the academic experience for students from diverse backgrounds."
     return example
 
 def generate_correction_html(answer):
@@ -38,10 +43,7 @@ def generate_correction_html(answer):
     generate_html(answer)
     correction_html = "<div style='background-color: #f0f0f0; padding: 30px 30px 30px 30px; word-wrap:break-word'>"
     data = extract_from_html()
-    print(data.count("diff_add"), data.count("diff_chg"), data.count("diff_sub"))
     data = data.replace("&nbsp;", " ")
-    correction_html += "<strong>We have " + str(data.count("diff_add")+data.count("diff_chg")+data.count("diff_sub")) + " suggestions for your essay! </strong><br>"
-    correction_html += '<span class="diff_add">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> = Added, &nbsp;&nbsp;&nbsp;<span class="diff_chg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> = Changed, &nbsp;&nbsp;&nbsp;<span class="diff_sub">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> = Deleted <br><br>'
     correction_html += data
     correction_html += "</div><br>"
     return correction_html
@@ -74,6 +76,9 @@ images_paths = st.file_uploader("Upload the images (optional, accept mutilple im
 
 if len(images_paths) > 0:
     # convert the image to text
+    images_paths = sorted(images_paths, key=lambda file: int(re.search(r'\d+', file.name).group() or 0))
+    for image_path in images_paths:    
+        print(image_path.name)
     image_description_arr = image_to_text(images_paths)
 
     # add the image description to the question
